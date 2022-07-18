a1db4e72e29e4a6b9695368217f207d3dd8c5595
Nick-2805 <nikolai-pavlov-1987@mail.ru>
2022-07-18 07:18:19 +0300

    init:start project

---
+++ src/Components/Spinner/spinner.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './spiner.css';
+
+const Spiner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-x1vjd7e8lum">
+            <div className="ldio-as1sh4wnkj8">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Spiner;
