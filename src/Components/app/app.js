77f85dc65adc42e3a2178d92adb42fdadf9699bc
Nick-2805 <nikolai-pavlov-1987@mail.ru>
2022-07-27 00:38:33 +0300

    feat: work props.children2

---
+++ src/Components/app/app.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+import Header from '../header';
+import ErrorBoundry from '../error-boundry';
+
+import Row from "../row/row";
+import ItemDetails, { Record } from "../item-details/item-details";
+import SwapiService from "../../services/SwapiService";
+
+import './app.css';
+
+export default class App extends Component {
+
+  swapiService = new SwapiService();
+
+  render() {
+
+    const { getPerson,
+            getStarship,
+            getPersonImage,
+            getStarshipImage } = this.swapiService;
+
+    const personDetails = (
+      <ItemDetails
+        itemId={11}
+        getData={getPerson}
+        getImageUrl={getPersonImage} >
+
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />
+
+      </ItemDetails>
+    );
+
+    const starshipDetails = (
+      <ItemDetails
+        itemId={5}
+        getData={getStarship}
+        getImageUrl={getStarshipImage}>
+
+      </ItemDetails>
+
+    );
+
+    return (
+      <ErrorBoundry>
+        <div className="stardb-app">
+          <Header />
+
+          <Row
+            left={personDetails}
+            right={starshipDetails} />
+        </div>
+      </ErrorBoundry>
+    );
+  }
+}
