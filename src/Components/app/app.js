78765df879663fc136b8ab6fbf1a908b21134001
Nick-2805 <nikolai-pavlov-1987@mail.ru>
2022-07-27 08:45:55 +0300

    feat: use React.cloneElement

--- src/Components/app/app.js
+++ src/Components/app/app.js
@@ -1,57 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Header from '../header';
 import ErrorBoundry from '../error-boundry';
 
 import Row from "../row/row";
-import ItemDetails, { Record } from "../item-details/item-details";
+import ItemDetails, {Record} from "../item-details/item-details";
 import SwapiService from "../../services/SwapiService";
 
 import './app.css';
 
 export default class App extends Component {
 
-  swapiService = new SwapiService();
+    swapiService = new SwapiService();
 
-  render() {
+    render() {
 
-    const { getPerson,
+        const {
+            getPerson,
             getStarship,
             getPersonImage,
-            getStarshipImage } = this.swapiService;
+            getStarshipImage
+        } = this.swapiService;
 
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage} >
+        const personDetails = (
+            <ItemDetails
+                itemId={11}
+                getData={getPerson}
+                getImageUrl={getPersonImage}>
 
-        <Record field="gender" label="Gender" />
-        <Record field="eyeColor" label="Eye Color" />
+                <Record field="gender" label="Gender"/>
+                <Record field="eyeColor" label="Eye Color"/>
 
-      </ItemDetails>
-    );
+            </ItemDetails>
+        );
 
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
+        const starshipDetails = (
+            <ItemDetails
+                itemId={5}
+                getData={getStarship}
+                getImageUrl={getStarshipImage}>
 
-      </ItemDetails>
+                <Record field="model" label="Model"/>
+                <Record field="length" label="Length"/>
+                <Record field="costInCredits" label="Credit"/>
 
-    );
 
-    return (
-      <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
+            </ItemDetails>
 
-          <Row
-            left={personDetails}
-            right={starshipDetails} />
-        </div>
-      </ErrorBoundry>
-    );
-  }
+        );
+
+        return (
+            <ErrorBoundry>
+                <div className="stardb-app">
+                    <Header/>
+
+                    <Row
+                        left={personDetails}
+                        right={starshipDetails}/>
+                </div>
+            </ErrorBoundry>
+        );
+    }
 }
